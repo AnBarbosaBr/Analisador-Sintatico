cc12606bcf02f3ed2afe177b746843cf8e542a50
Yamesh <seupera@hotmail.com>
2018-06-28 01:27:18 -0300

    Esqueleto de um exemplo da aplicação. Pode e deve ser reescrito.

--- src/draft.c
+++ src/draft.c
@@ -2,7 +2,23 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
+#define PROCESSANDO "em processamento"
+#define NOVO_TOKEN "estado inicial"
+#define ERRO_TOKEN "ocorreu um erro"
+
+#define IDENTIFICADOR "IDENTIFICADOR"
+#define NUMERO "NUMERO"
+
+#define PONTO_VIRGULA "PONTO_VIRGULA"
+#define VIRGULA "VIRGULA"
+#define ABRE_PARENTESES "ABRE_PARENTESES"
+#define FECHA_PARENTESES "FECHA_PARENTESES"
+#define ABRE_CHAVES "ABRE_CHAVES"
+#define FECHA_CHAVES "FECHA_CHAVES"
+#define ATRIBUICAO "ATRIBUICAO"
+#define OPERADOR "OPERADOR"
 
 typedef struct informacoes {
     char token[255]; // ajustar tamanho do char para corresponder ao maior token,
@@ -13,7 +29,7 @@ typedef struct informacoes {
 
 void analyse(char *arquivo);
 void analiseLexica(FILE *fb);
-void analisaEspacos(char ch, struct informacoes *token, int *numeroDeTokens);
+void consomeEspacos(char ch, struct informacoes *token, int *numeroDeTokens);
 informacoes analisaNaoEspacos(char ch, struct informacoes *token, int *numeroDeTokens);
 void imprimeToken(informacoes token);
 
@@ -21,6 +37,7 @@ void imprimeToken(informacoes token);
 
 int main(int argc, char **argv){
 
+    printf(" Iniciando operacao \n");
     char *fileDir;
     if(argc > 1)    { // Foi informado um caminho.
         fileDir = argv[1];
@@ -50,32 +67,43 @@ void analyse(char *arquivo) {
 }
 
 void analiseLexica(FILE *fp) {
-    
     char ch;
     informacoes tokenContador;
     informacoes tokenRetornado;
     int numeroDeTokens = 0;
     tokenContador.linha = 1;  // linhas são numeradas de 1 em diante.
     tokenContador.coluna = 1; // colunas são numeradas de 1 em diante.
+    strcpy(tokenContador.token, NOVO_TOKEN); // token no estado inicial.
+
     while((ch = fgetc(fp)) != EOF)
     {
-        if(isspace(ch)){
-            analisaEspacos(ch, &tokenContador, &numeroDeTokens);
-        } else{
-            informacoes tokenRetornado;
-            tokenRetornado = analisaNaoEspacos(ch, &tokenContador, &numeroDeTokens);
-            if(strcmp(tokenRetornado.token , "em processamento") != 0){
-                imprimeToken(tokenRetornado);
-            }
+        // Se estamos processando algum token
+        if(strcmp(tokenContador.token , NOVO_TOKEN) != 0){
+            if(isspace(ch)){ // Encontramos um espaço, 
+                    //terminamos o processamento.
+                    imprimeToken(tokenContador);
+                    // voltamos a buscar outro token
+                    setToken(&tokenContador, NOVO_TOKEN, "");
+            } 
+        }  
 
 
-        }
-        
+        // Se estamos procurando um novo token, 
+        if( (strcmp(tokenContador.token, NOVO_TOKEN) == 0) ) {
+            // Se encontramos espaços
+            if(isspace(ch)){
+                    consomeEspacos(ch, &tokenContador, &numeroDeTokens);
+            } else{ // Se encontramos caractere
+                tokenContador = analisaNaoEspacos(ch, &tokenContador, &numeroDeTokens);
+                // Se o token não estiver mais PROCESSANDO, imprimimos o resultado.
+                  
+        }}
     }
     printf("Linhas %d e Colunas %d\n" ,tokenContador.linha, tokenContador.coluna);
 }
 
-void analisaEspacos(char ch, informacoes *tokens, int *numeroDeTokens)
+
+void consomeEspacos(char ch, informacoes *tokens, int *numeroDeTokens)
 {
     if(ch == '\n')
     {
@@ -85,18 +113,94 @@ void analisaEspacos(char ch, informacoes *tokens, int *numeroDeTokens)
         tokens->coluna++;
     }
 }
-informacoes analisaNaoEspacos(char ch, informacoes *tokens, int *numeroDeTokens)
+/*
+informacoes stubInfo(informacoes *tokens)
 {
-    tokens->coluna++;
-    informacoes tokenARetornar = { .token = "em processamento", 
+    informacoes tokenARetornar = { .token = "PROCESSANDO", 
                                     .lexema = "",
                                     .linha = tokens->linha,
                                     .coluna = tokens->coluna
                                     };
     return tokenARetornar;
 }
+*/
+void setTokenValue(informacoes *token, char *novoValor)
+{
+    strcpy(token->token, novoValor);
+}
+
+void appendToLexema(informacoes *token, char aAnexar)
+{
+    // Criar uma maneira de "adicionar" o caractere ao lexema.
+    // Essa não funciona direito.
+    printf("Anexando %c ao lexema %c\n", aAnexar, *token->lexema);
+    strcat(token->lexema, &aAnexar);
+}
+informacoes analisaNaoEspacos(char ch, informacoes *tokens, int *numeroDeTokens)
+{
+    assert(!isspace(ch));
+    if(strcmp(tokens->token, NOVO_TOKEN)==0){ //estado inicial
+        if(isalnum(ch))
+        {
+            processaNewAlfaNumerico(ch, tokens);
+        } else
+        {
+            processaNewSimbolo(ch, tokens);
+        }
+    } else
+    {
+        if(isalnum(ch))
+        {
+            processaNewAlfaNumerico(ch, tokens);
+        } else
+        {
+            processaNewSimbolo(ch, tokens);
+        } 
+    }
+    printf("\n processamento %c  -  ", ch);
+    tokens->coluna++;
+
+    return *tokens;
+}
+
+void setToken(informacoes *token, char *newToken, char *newLexema)
+{
+    strcpy(token->token , newToken);
+    strcpy(token->lexema , newLexema);
+}
+void processaNewAlfaNumerico(char ch, informacoes *token)
+{
+    if(isalpha(ch))
+    {
+        setTokenValue(token, IDENTIFICADOR);
+        appendToLexema(token, ch);
+    }
+    if(isdigit(ch)){
+        setTokenValue(token, NUMERO);
+        appendToLexema(token, ch);
+    }
+}
+
+void processaNewSimbolo(char ch, informacoes *token)
+{
+    switch (ch)
+    {
+        case ';': setToken(token, PONTO_VIRGULA , &ch); break;
+        case '(': setToken(token, ABRE_PARENTESES , &ch); break;
+        case ')': setToken(token, FECHA_PARENTESES , &ch); break;
+        case '{': setToken(token, ABRE_CHAVES, &ch); break;
+        case '}': setToken(token, FECHA_CHAVES, &ch); break;
+        case ',': setToken(token, VIRGULA, &ch); break;
+        case '=': setToken(token, ATRIBUICAO, &ch); break;
+        case '-': setToken(token, OPERADOR, &ch); break;
+        case '*': setToken(token, OPERADOR, &ch); break;
+        case '/': setToken(token, OPERADOR, &ch); break;
+        case '+': setToken(token, OPERADOR, &ch); break;
+    }
+}
+
 
 void imprimeToken(informacoes token){
-    printf("%s@%s@%d%d\n", token.token, token.lexema,
+    printf("%s @ %s @ %d @ %d\n", token.token, token.lexema,
                              token.linha, token.coluna);
 }
