93f3ee320179fc665c84de88e0bc97145db8aaed
Yamesh <seupera@hotmail.com>
2018-07-29 16:58:32 -0300

    Linguagem recursiva simples passando nos testes Sucesso1, Falha1, Falha2.

--- src/AnalisadorSintatico/recursiva.c
+++ src/AnalisadorSintatico/recursiva.c
@@ -31,19 +31,53 @@ void AdvanceToken();
 int ehFinal();
 void throwSyntaxError();
 void AddStartToSyntaxTree(char* tokenName);
-void AddEntToSyntaxTree(char* tokenName);
+void AddEndToSyntaxTree(char* tokenName);
+void AddLexemaToSyntaxTree(Token token);
+
 Token* AddToTokenList(char *tokenName, char *tokenLexema);
+int tokenEh(Token t, char* tokenName);
 
-
-int main(int argc, char **argv) {
-
+void casoDeTesteSucesso(){
+    AddToTokenList("(", "(");
     AddToTokenList("a", "1"); 
     AddToTokenList("+", "+");
     AddToTokenList("a", "2");
+    AddToTokenList(")", ")");
+}
+
+void casoDeTeste_Falha1(){
+    //AddToTokenList("(", "(");
+    AddToTokenList("a", "1"); 
+    AddToTokenList("+", "+");
+    AddToTokenList("a", "2");
+    AddToTokenList(")", ")");
+}
+
+void casoDeTeste_Falha2(){
+    AddToTokenList("(", "(");
+    AddToTokenList("a", "1"); 
+    AddToTokenList("+", "+");
+    AddToTokenList("a", "2");
+    //AddToTokenList(")", ")");
+}
+
+void casoDeTeste_Falha3(){
+    AddToTokenList("(", "(");
+    AddToTokenList("a", "1"); 
+    AddToTokenList("+", "+");
+    //AddToTokenList("a", "2");
+    AddToTokenList(")", ")");
+}
+
+int main(int argc, char **argv) {
+
+    casoDeTeste_Falha2();
     // Sempre começa com S.
     AdvanceToken();
     compilaS();
-    
+    if(!ehFinal()){
+        throwSyntaxError();
+    }
 
 
 }
@@ -52,12 +86,13 @@ int main(int argc, char **argv) {
 void compilaS(){
     Token token = GetCurrentToken();
       AddStartToSyntaxTree("S");
-    if(tokenEh(token, '(')){ // seguimos a regra "S-> ( S + F )"
-        eat(GetCurrentToken(), '(');
+    if(tokenEh(token, "(")){ // seguimos a regra "S-> ( S + F )"
+        eat(GetCurrentToken(), "(");
         compilaS();
-        eat(GetCurrentToken(), '+');
+        eat(GetCurrentToken(), "+");
         compilaF();
-        eat(GetCurrentToken(), ')');
+        //printf("Analisando ultimo item: %s", GetCurrentToken().lexema);
+        eat(GetCurrentToken(), ")");
     } else{
         compilaF();
     }
@@ -70,8 +105,8 @@ void compilaS(){
 void compilaF(){
     Token token = GetCurrentToken();
     AddStartToSyntaxTree("F");
-    if(tokenEh(token, 'a')) {
-        eat(GetCurrentToken(), 'a');
+    if(tokenEh(token, "a")) {
+        eat(GetCurrentToken(), "a");
     } else {
         throwSyntaxError();
     }
@@ -83,8 +118,8 @@ void eat(Token token, char* token_esperado)
 {
     if(tokenEh(token, token_esperado)) {
         AddStartToSyntaxTree(token.tokenName);
-        AddLexemaToSyntaxTree(GetCurrentLexema());
-        AddEndToSyntaxTree(token);
+        AddLexemaToSyntaxTree(token);
+        AddEndToSyntaxTree(token.tokenName);
         AdvanceToken();
     }
 
@@ -98,13 +133,17 @@ Token GetCurrentToken(){
     return listaDeTokens[currentToken];
 }
 
+// Retorna verdadeiro se não há diferenças entre t.tokenName e tokenName.
+int tokenEh(Token t, char* tokenName){
+    return (strcmp(t.tokenName, tokenName) == 0);
+}
 
 void AdvanceToken(){
     currentToken++;
-    if(ehFinal()){
-        print("Arquivo analisado com sucesso. Essa sintaxe é válida.");
-        exit(EXIT_SUCCESS);
-    }
+    // if(ehFinal()){
+    //     printf("Arquivo analisado com sucesso. Essa sintaxe eh valida.");
+    //     exit(EXIT_SUCCESS);
+    // }
 }
 
 int ehFinal(){
@@ -113,22 +152,25 @@ int ehFinal(){
 
 
 void throwSyntaxError(){
-    fprintf(stderr, "%s\n", "Linguagem não aceita.");
+    fprintf(stderr, "%s\n", "Linguagem nao aceita.");
     exit(EXIT_FAILURE);;
 }
 
 
 void AddStartToSyntaxTree(char* tokenName){
-    print("(Start-");
-    print(tokenName);
-    println(")");
+    printf("<");
+    printf(tokenName);
+    printf("> \n");
 }
-void AddEntToSyntaxTree(char* tokenName){
-    print("(End-");
-    print(tokenName);
-    println(")");
+void AddEndToSyntaxTree(char* tokenName){
+    printf("</");
+    printf(tokenName);
+    printf(">\n");
 }
 
+void AddLexemaToSyntaxTree(Token token){
+    printf("%s\n", token.lexema);
+}
 Token* AddToTokenList(char *tokenName, char *tokenLexema){
     Token *tok = malloc(sizeof(Token));
     strcpy(tok->tokenName, tokenName);
